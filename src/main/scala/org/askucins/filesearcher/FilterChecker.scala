615ef5818b006efbb6550dcb23d0a1767a08c1fc
Adam Skucinski <askucins@gmail.com>
2019-04-14 13:31:48 +0200

    applied infix notation of style convention

--- src/main/scala/org/askucins/filesearcher/FilterChecker.scala
+++ src/main/scala/org/askucins/filesearcher/FilterChecker.scala
@@ -1,13 +1,17 @@
 package org.askucins.filesearcher
 
 class FilterChecker(filter: String) {
-  def matches(content: String): Boolean = content.contains(filter)
+  def matches(content: String): Boolean = content contains filter
 
   def isFileObject(ioObject: IOObject): Boolean = ioObject.isInstanceOf[FileObject]
 
-  def findMatchedFiles(ioObjects: List[IOObject]): List[IOObject] = {
+  def findMatchedFiles(ioObjects: List[IOObject]): List[IOObject] =
     for (ioObject <- ioObjects
-         if matches(ioObject.name) && isFileObject(ioObject))
+         if matches(ioObject.name)
+         if isFileObject(ioObject))
       yield ioObject
-  }
+}
+
+object FilterChecker {
+  def apply(filter: String): FilterChecker = new FilterChecker(filter)
 }
--- src/main/scala/org/askucins/filesearcher/IOObject.scala
+++ src/main/scala/org/askucins/filesearcher/IOObject.scala
@@ -4,6 +4,6 @@ trait IOObject {
   val name: String
 }
 
-class FileObject(val name: String) extends IOObject {}
+class FileObject(val name: String) extends IOObject
 
-class DirectoryObject(val name: String) extends IOObject {}
+class DirectoryObject(val name: String) extends IOObject
