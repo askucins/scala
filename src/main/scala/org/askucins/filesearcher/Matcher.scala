338b6c1baa7c6d80eb615ac61676f788da42061b
Adam Skucinski <askucins@gmail.com>
2019-04-15 02:58:44 +0200

    misc changes

--- src/main/scala/org/askucins/filesearcher/Matcher.scala
+++ src/main/scala/org/askucins/filesearcher/Matcher.scala
@@ -7,20 +7,22 @@ class Matcher(filter: String, val rootLocation: String = new File(".").getCanoni
 
   def execute(): List[String] = {
 
-    //TODO Replace this with a real tail recursion
-    def findMatchedFiles(root: IOObject, accumulator: List[IOObject]): List[IOObject] = {
+    //TODO Replace this with tail recursion
+
+    def findMatchedFiles(root: IOObject): List[IOObject] = {
+      println(root.name)
       root match {
-        case file: FileObject if FilterChecker(filter) matches file.name => accumulator ++ List(file)
+        case file: FileObject if FilterChecker(filter) matches file.name => List(file)
         case directory: DirectoryObject =>
           if (checkSubFolders)
-            (directory.children() map (it => findMatchedFiles(it, accumulator))).reduce(_ ++ _)
+            (directory.children() map (it => findMatchedFiles(it))).reduce(_ ++ _)
           else
-            accumulator ++ (FilterChecker(filter) findMatchedFiles directory.children())
+            FilterChecker(filter) findMatchedFiles directory.children()
         case _ => List()
       }
     }
 
-    val matchedFiles = findMatchedFiles(rootIOObject, List())
+    val matchedFiles = findMatchedFiles(rootIOObject)
     matchedFiles map (ioObject => ioObject.name)
   }
 }
