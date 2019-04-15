1df2dac705f1def7eacacbeaeabc66a9cc12d8a7
Adam Skucinski <askucins@gmail.com>
2019-04-15 02:07:44 +0200

    added basic recursive solution - without tail recursion though

--- src/main/scala/org/askucins/filesearcher/Matcher.scala
+++ src/main/scala/org/askucins/filesearcher/Matcher.scala
@@ -6,13 +6,20 @@ class Matcher(filter: String, val rootLocation: String = new File(".").getCanoni
   val rootIOObject: IOObject = FileConverter.convertToIOObject(new File(rootLocation))
 
   def execute(): List[String] = {
-    val matchedFiles = rootIOObject match {
-      case file: FileObject if FilterChecker(filter) matches file.name => List(file)
-      case directory: DirectoryObject =>
-        FilterChecker(filter) findMatchedFiles directory.children()
-      case _ => List()
+
+    def findMatchedFiles(root: IOObject): List[IOObject] = {
+      root match {
+        case file: FileObject if FilterChecker(filter) matches file.name => List(file)
+        case directory: DirectoryObject =>
+          if (searchSubFolders)
+            (directory.children() map (it => findMatchedFiles(it))).reduce(_ ++ _)
+          else
+            FilterChecker(filter) findMatchedFiles directory.children()
+        case _ => List()
+      }
     }
 
+    val matchedFiles = findMatchedFiles(rootIOObject)
     matchedFiles map (ioObject => ioObject.name)
   }
 }
