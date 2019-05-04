a8cfeb461c46ec7edf9524c7fa5471ce33c11ce0
Adam Skucinski <askucins@gmail.com>
2019-05-04 15:47:25 +0200

    added annotation to control tail-recursion

--- src/main/scala/org/askucins/filesearcher/Matcher.scala
+++ src/main/scala/org/askucins/filesearcher/Matcher.scala
@@ -2,26 +2,13 @@ package org.askucins.filesearcher
 
 import java.io.File
 
+import scala.annotation.tailrec
+
 class Matcher(filter: String, val rootLocation: String = new File(".").getCanonicalPath, checkSubFolders: Boolean = false) {
   val rootIOObject: IOObject = FileConverter.convertToIOObject(new File(rootLocation))
 
   def execute(): List[String] = {
-    //Naive implementation
-    /*
-    def recursiveMatchNaive(root: IOObject): List[IOObject] = {
-      root match {
-        case file: FileObject if FilterChecker(filter) matches file.name => List(file)
-        case directory: DirectoryObject =>
-          if (checkSubFolders)
-            (directory.children() map (it => recursiveMatchNaive(it))).reduce(_ ++ _)
-          else
-            FilterChecker(filter) findMatchedFiles directory.children()
-        case _ => List()
-      }
-    }
-    val matchedFiles = recursiveMatchNaive(rootIOObject)
-    */
-
+    @tailrec
     def recursiveMatch(files: List[IOObject], currentList: List[FileObject]): List[FileObject] = {
       files match {
         case List() => currentList
@@ -43,6 +30,22 @@ class Matcher(filter: String, val rootLocation: String = new File(".").getCanoni
         else FilterChecker(filter) findMatchedFiles directory.children()
       case _ => List()
     }
+
+    //Naive implementation
+    /*
+    def recursiveMatchNaive(root: IOObject): List[IOObject] = {
+      root match {
+        case file: FileObject if FilterChecker(filter) matches file.name => List(file)
+        case directory: DirectoryObject =>
+          if (checkSubFolders)
+            (directory.children() map (it => recursiveMatchNaive(it))).reduce(_ ++ _)
+          else
+            FilterChecker(filter) findMatchedFiles directory.children()
+        case _ => List()
+      }
+    }
+    val matchedFiles = recursiveMatchNaive(rootIOObject)
+    */
 
     matchedFiles map (ioObject => ioObject.name)
   }
